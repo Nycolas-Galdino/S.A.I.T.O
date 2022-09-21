40e19da1a75e0b34b277d6ca09d91fd030dbe909
npg <Nycolas Pimentel Galdino>
2022-09-21 14:59:58 -0300

    {Project: "S.A.I.T.O", Version: 1.1, Publication date: 20-09-2022,
    Active functions [
    "Register user",
    "Read user",
    "Listen to command",
    "Speak",
    "Program Voice",
    "Open page of web"
    "Turn op the volume"
    "Turn down the volume"
    
    ]
    }

---
+++ config.py
@@ -0,0 +1,39 @@
+import os
+from time import sleep
+
+import pyttsx3
+
+tts = pyttsx3.init()
+
+voices = tts.getProperty("voices")
+x=0
+
+for i in voices:
+    if "Saito" in str(i):
+        print(i)
+        break
+
+    if i == voices[len(voices) - 1]:
+        os.system("db/Saito.reg")
+        break
+
+    x += 1
+
+print("Estou configurando minha voz...")
+sleep(3)
+tts.setProperty("voice", voices[x].id)
+print("Pronto! Agora você consegue me ouvir melhor!")
+tts.say("Pronto! Agora você consegue me ouvir melhor!")
+tts.runAndWait()
+
+boasVindas = """Olá, meu nome é Saito, prazer em lhe conhecer!
+
+Sou uma IA desenvolvida para lhe fornecer o maior auxílio possível.
+
+Estou programado para ouvir voce através de seu MICROFONE, então, conecte um fone de ouvido com microfone ou um microfone mesmo e vamos lá!
+
+Por favor, me diga seu nome!"""
+
+print(boasVindas)
+tts.say(boasVindas)
+tts.runAndWait()
--- main.py
+++ main.py
@@ -1,34 +1,46 @@
 from time import sleep
-from gtts import gTTS
+import pyttsx3
 import speech_recognition as srLib
-from playsound import playsound
+import webbrowser as wb
 
 
 class Saito:
     def __init__(self):
         self.nome = "Saito Shion Cipriano"
+        self.microfone = None
+        self.volume = 0.5
 
         if self.lerUsuario() == "":
             self.gravarUsuario()
 
         self.usuario = self.lerUsuario()
 
-        welcome = f"Seja bem vindo(a) novamente {self.user}!"
-        print(welcome)
+        welcome = f"Olá novamente {self.user}!"
         self.falar(welcome)
+        self.inicializar()
 
-        self.inicializar(self.usuario)
+    def inicializar(self):
+        for i in range(5):
+            frase = self.ouvir()
 
-    def inicializar(self, nome):
-        pass
+            if i == 5:
+                self.falar("Notei uma certa inatividade no sistema, irei encerrar por agora, okay? Até mais!")
+
+            if "fechar" in frase:
+                self.falar("Tudo bem, fico por aqui então! Até logo!")
+                break
+
+            if "pesquisar" in frase:
+                self.abrirPagina(frase.replace("pesquisar",""))
+                sleep(5)
+                i=0
 
     def gravarUsuario(self):
-        self.boas_vindas_txt = open("./db/Boas vindas.txt", "rt", encoding="utf-8")
-        print(self.boas_vindas_txt.read())
-        self.boas_vindas_txt.close()
+        import config
+        config
 
         self.user_txt = open("./db/User.txt", "wt", encoding="utf-8")
-        user = self.ouvir(frase="Qual é o seu nome?")
+        user = self.ouvir()
         print(self.user_txt.write(user))
         self.user_txt.close()
 
@@ -49,25 +61,67 @@ class Saito:
                 sleep(1)
                 audio = mic.listen(gravacao)
                 texto = mic.recognize_google(audio, language="pt-BR")
-                print(texto)
                 return texto
             except srLib.UnknownValueError:
                 print(erro)
 
     def falar(self, texto):
+        print(texto)
         try:
-            tts = gTTS(texto, lang="pt-BR")
-            localAudio = "./db/temp_audio.mp3"
-            tts.save(localAudio)
-            playsound(localAudio)
+            if self.microfone is None:
+                self.microfone = self.configurarMicrofone()
+
+                #Velocidade do som
+            if len(texto) <= 50:
+                self.voz.setProperty("rate",200)
+            elif len(texto) <= 100:
+                self.voz.setProperty("rate",250)
+            elif len(texto) <= 200:
+                self.voz.setProperty("rate",300)
+            else:
+                self.voz.setProperty("rate", 350)
+
+                #Volume
+
+            if self.volume == 1:
+                self.falar("O volume está no máximo")
+
+            if self.volume == 0.25:
+                self.falar("O volume está no mínimo")
+
+            if texto == "Aumentar o volume" & self.volume < 1:
+                self.volume +=  0.25
+                self.falar("O volume está em {}%".format(100*(self.volume/1)))
+
+            if texto == "Diminuir o volume" & self.volume > 0.25:
+                self.volume -=  0.25
+                self.falar("O volume está em {}%".format(100*(self.volume/1)))
+
+            self.voz.setProperty("voice",self.microfone)
+            self.voz.setProperty("volume",self.volume)
+            self.voz.say(texto)
+            self.voz.runAndWait()
         except:
-            print(f"""Erro no código""")
+            print(f"""Erro ao falar""")
+
+    def configurarMicrofone(self):
+        self.voz = pyttsx3.init("sapi5")
+        self.vozes = self.voz.getProperty("voices")
+        x = 0
+
+        for i in self.vozes:
+            if "Saito" in str(i):
+                break
+            x += 1
+
+        return self.vozes[x].id
 
     def abrirSoftware(self):
         pass
 
-    def abrirPagina(self):
-        pass
+    def abrirPagina(self, pagina):
+        wb.open(fr"www.google.com/search?q={pagina}".replace(" ","%20"))
+        self.falar("Abrino pesquisa na web")
 
 if __name__ == '__main__':
     Saito()
